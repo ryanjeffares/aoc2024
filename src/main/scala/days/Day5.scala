57cad292b27f844fef48fce2f6c0438f7bf1425f
ryanjeffares <ryandjeffares@gmail.com>
2024-12-05 19:22:50 +0000

    day 5

--- src/main/scala/days/Day5.scala
+++ src/main/scala/days/Day5.scala
@@ -5,17 +5,57 @@ import scala.util.Using
 
 class Day5 extends Day {
   override def part1(): Unit =
-    val rules = Using(Source.fromFile("src/main/resources/day5_rules.txt")) { source =>
+    val rules = getRules
+
+    println(Using(Source.fromFile("src/main/resources/day5_updates.txt")) { source =>
       source.getLines()
-        .map(_.split('|').map(_.toInt))
-        .toArray
-        .groupMapReduce(a => a(0))(a => List(a(1)))((l1, l2) => l1.concat(l2))
-    }.get
+        .map(_.split(',').map(_.toInt))
+        .filter(update => isOrdered(update, rules))
+        .map(update => update(update.length / 2))
+        .sum
+    }.get)
+
+  override def part2(): Unit =
+    val rules = getRules
     
     println(Using(Source.fromFile("src/main/resources/day5_updates.txt")) { source =>
       source.getLines()
-    })
+        .map(_.split(',').map(_.toInt))
+        .filter(update => !isOrdered(update, rules))
+        .map { update =>
+          update.indices.iterator.foreach { i =>
+            update.indices.drop(i + 1).foreach { j =>
+              rules.get(update(j)) match {
+                case Some(s) if s.contains(update(i)) =>
+                  val temp = update(i)
+                  update.update(i, update(j))
+                  update.update(j, temp)
+                case Some(_) => ()
+                case None => ()
+              }
+            }
+          }
 
+          update(update.length / 2)
+        }
+        .sum
+    }.get)
 
-  override def part2(): Unit = ()
+  private def getRules: Map[Int, Set[Int]] =
+    Using(Source.fromFile("src/main/resources/day5_rules.txt")) { source =>
+      source.getLines()
+        .map(_.split('|').map(_.toInt))
+        .toArray
+        .groupMapReduce(a => a(0))(a => Set(a(1)))((s1, s2) => s1.union(s2))
+    }.get
+
+  private def isOrdered(update: Array[Int], rules: Map[Int, Set[Int]]): Boolean =
+    update.indices.iterator.forall { i =>
+      update.indices.drop(i + 1).forall { j =>
+        rules.get(update(j)) match {
+          case Some(s) => !s.contains(update(i))
+          case None => false
+        }
+      }
+    }
 }
